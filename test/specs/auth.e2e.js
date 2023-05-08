03e76865400bd0e1d91a17c7e1a185834bf46bb9
David Ward <spy.sappin.your.sentry@gmail.com>
2023-05-08 15:27:22 -0700

    Update inventory page expected title

--- test/specs/auth.e2e.js
+++ test/specs/auth.e2e.js
@@ -1,76 +1,76 @@
-const LoginPage = require("../pageobjects/login.page");
-const InventoryPage = require("../pageobjects/inventory.page");
-const config = require("config");
+const LoginPage = require('../pageobjects/login.page');
+const InventoryPage = require('../pageobjects/inventory.page');
+const config = require('config');
 
-describe("Auth", () => {
+describe('Auth', () => {
   beforeEach(async () => {
     await LoginPage.open();
   });
-  context("basic user @smoke", () => {
-    it("should login with valid credentials", async () => {
+  context('basic user @smoke', () => {
+    it('should login with valid credentials', async () => {
       await LoginPage.login(
         config.users.standard.user,
         config.users.standard.pass
       );
       await expect(InventoryPage.secondaryTitle).toHaveTextContaining(
-        "PRODUCTS"
+        'Products'
       );
     });
-    it("should not login with invalid credentials", async () => {
-      await LoginPage.login(config.users.standard.user, "a_wrong_password");
+    it('should not login with invalid credentials', async () => {
+      await LoginPage.login(config.users.standard.user, 'a_wrong_password');
       await expect(LoginPage.errorMessageText).toHaveTextContaining(
-        "Username and password do not match any user in this service"
+        'Username and password do not match any user in this service'
       );
     });
   });
-  context("locked out user", () => {
-    it("should login with valid credentials but locked out", async () => {
+  context('locked out user', () => {
+    it('should login with valid credentials but locked out', async () => {
       await LoginPage.login(config.users.locked.user, config.users.locked.pass);
       await expect(LoginPage.errorMessageText).toHaveTextContaining(
-        "Sorry, this user has been locked out"
+        'Sorry, this user has been locked out'
       );
     });
-    it("should not login with invalid credentials", async () => {
-      await LoginPage.login(config.users.locked.user, "a_wrong_password");
+    it('should not login with invalid credentials', async () => {
+      await LoginPage.login(config.users.locked.user, 'a_wrong_password');
       await expect(LoginPage.errorMessageText).toHaveTextContaining(
-        "Username and password do not match any user in this service"
+        'Username and password do not match any user in this service'
       );
     });
   });
-  context("problem user", () => {
-    it("should login with valid credentials but locked out", async () => {
+  context('problem user', () => {
+    it('should login with valid credentials but locked out', async () => {
       await LoginPage.login(
         config.users.problem.user,
         config.users.problem.pass
       );
       await expect(InventoryPage.secondaryTitle).toHaveTextContaining(
-        "PRODUCTS"
+        'Products'
       );
     });
-    it("should not login with invalid credentials", async () => {
-      await LoginPage.login(config.users.problem.user, "a_wrong_password");
+    it('should not login with invalid credentials', async () => {
+      await LoginPage.login(config.users.problem.user, 'a_wrong_password');
       await expect(LoginPage.errorMessageText).toHaveTextContaining(
-        "Username and password do not match any user in this service"
+        'Username and password do not match any user in this service'
       );
     });
   });
-  context("performance issue user", () => {
-    it("should login with valid credentials", async () => {
+  context('performance issue user', () => {
+    it('should login with valid credentials', async () => {
       await LoginPage.login(
         config.users.performance_problem.user,
         config.users.performance_problem.pass
       );
       await expect(InventoryPage.secondaryTitle).toHaveTextContaining(
-        "PRODUCTS"
+        'Products'
       );
     });
-    it("should not login with invalid credentials", async () => {
+    it('should not login with invalid credentials', async () => {
       await LoginPage.login(
         config.users.performance_problem.user,
-        "a_wrong_password"
+        'a_wrong_password'
       );
       await expect(LoginPage.errorMessageText).toHaveTextContaining(
-        "Username and password do not match any user in this service"
+        'Username and password do not match any user in this service'
       );
     });
   });
