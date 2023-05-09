44f696a83bce8e12823d28a3bcad698ddc8eda88
David Ward <spy.sappin.your.sentry@gmail.com>
2023-05-08 17:42:43 -0700

    Add basic unit tests for ints, phase 3

--- test/specs/unit/util.unit.js
+++ test/specs/unit/util.unit.js
@@ -14,7 +14,15 @@ describe('util.js', () => {
       const result = util.isSortedHiToLow(list);
       expect(result).to.equal(true);
     });
+    it('returns true when list is descending: saucedemo results', () => {
+      // Obtained via console.log on the `items` variable (in test/specs/e2e/inventory.e2e.js, Line 43)
+      // Corresponds to the value returned from the "Price (high to low)" test
+      const list = [49.99, 29.99, 15.99, 15.99, 9.99, 7.99];
+      const result = util.isSortedHiToLow(list);
+      expect(result).to.equal(true);
+    });
   });
+  //[ 49.99, 29.99, 15.99, 15.99, 9.99, 7.99 ]
   context('isSortedLowToHi()', () => {
     it('returns true when list is ascending', () => {
       const list = [123, 456];
@@ -25,6 +33,13 @@ describe('util.js', () => {
       const list = [456, 123];
       const result = util.isSortedLowToHi(list);
       expect(result).to.equal(false);
+    });
+    it('returns true when list is ascending: saucedemo results', () => {
+      // Obtained via console.log on the `items` variable (in test/specs/e2e/inventory.e2e.js, Line 43)
+      // Corresponds to the value returned from the "Price (low to high)" test
+      const list = [7.99, 9.99, 15.99, 15.99, 29.99, 49.99];
+      const result = util.isSortedLowToHi(list);
+      expect(result).to.equal(true);
     });
   });
 });
